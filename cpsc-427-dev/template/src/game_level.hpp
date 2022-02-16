480700d93e4764cf878e92e9718b71642bf13f6b
unknown <shiyuchen6688@gmail.com>
2022-02-16 11:18:42 -0800

    game_level.hpp design done for GameLevel class

--- cpsc-427-dev/template/src/game_level.hpp
+++ cpsc-427-dev/template/src/game_level.hpp
@@ -3,4 +3,10 @@ class GameLevel
 public:
 	int level;
 	vec2 character_position; // TODO: for the future, maybe save character position
+	// load level from file
+	void loadlLevelFromFile(const char* file);
+	// save level to file
+	void saveLevelToFile(const char* file);
+	// reset level
+	void resetToInitialLevel(const char* file);
 };
